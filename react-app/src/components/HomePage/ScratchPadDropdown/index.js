e66d326917226f1ebcb04bc068d59e35560fe448
justin chau <jchau623@gmail.com>
2022-09-12 13:00:31 -0400

    scratchpad fixed

--- react-app/src/components/HomePage/ScratchPadDropdown/index.js
+++ react-app/src/components/HomePage/ScratchPadDropdown/index.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 // import { getNotebooks } from "../../../store/notebooks"
 import './ScratchPadDropdown.css'
 
-export default function ScratchPadDropdown({ setShowDropdown,showDropdown, notebooks, handleClickNotebook }) {
+export default function ScratchPadDropdown({ setShowDropdown, showDropdown, notebooks, handleClickNotebook }) {
 
+    useEffect(() => {
+        if (!showDropdown) return
+        const handleClick = (e) => {
+            setShowDropdown(false);
 
-    const handleClick = (e) => {
-        setShowDropdown(!showDropdown);
+        };
+        document.addEventListener('click', handleClick)
+
+        return () => document.removeEventListener('click', handleClick)
         // this toggles it true/false. you only set it to true, which is why it never becomes false again
-    };
+    }, [showDropdown])
+
     return (
         <>
             <div className='scratchpad-dropdown'>
                 <i
-                    onClick={handleClick}
+                    onClick={setShowDropdown}
                     id='triple-dot'
                     className="fa-light fa-ellipsis"
-                    />
+                />
                 <div className='scratchpad-notebooks-div'>
                     {showDropdown && notebooks.map((notebook, index) => (
                         <p key={index} onClick={() => handleClickNotebook(notebook.id)} className='scratchpad-notebooks'>Add to {notebook.name}</p>
